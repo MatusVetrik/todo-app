17d94eef2a6e9338c05674b4d5576d73a36a0b62
MatusVetrik <xvetrik@stuba.sk>
2022-06-29 10:43:34 +0200

    feat: all providers and router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import {ThemeProvider} from "@emotion/react";
+import {createTheme} from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {Provider} from "react-redux";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import App from "./App";
+import {TodoListForm} from "./components/forms/TodoListForm";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import Header from "./components/UI/Header";
+import "./index.css";
+import {store} from "./redux/store";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#7D4CDB",
+    },
+    secondary: {
+      main: "#F8F8F8",
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route
+            path="/todos"
+            element={
+              <>
+                <Header />
+                <App />
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <TodoListForm />
+              </>
+            }
+          />
+        </Routes>
+      </ThemeProvider>
+    </Provider>
+  </BrowserRouter>
+);
