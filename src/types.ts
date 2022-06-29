d44bc810cac46a48ca3a6d39a836428189777d6a
MatusVetrik <xvetrik@stuba.sk>
2022-06-29 10:43:11 +0200

    feat: ts todos types

---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export interface Todo {
+  id: number;
+  title: string;
+  text: string;
+  deadline: string;
+  completed: boolean;
+}
+
+export interface Todos {
+  id: number;
+  name: string;
+  todos: Array<Todo>;
+}
+
+export interface TodosList {
+  todosList: Array<Todos>;
+}
+
+export interface TodosResponse extends TodosList {
+  id: number;
+}
