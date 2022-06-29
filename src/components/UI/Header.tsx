12ff4a510b2243a7618405bf7f16bff6ba460d6f
MatusVetrik <xvetrik@stuba.sk>
2022-06-29 10:44:27 +0200

    feat: header with navbar

---
+++ src/components/UI/Header.tsx
@@ -0,0 +1,31 @@
+import {
+  AppBar,
+  Box,
+  createTheme,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import Toolbar from "@mui/material/Toolbar";
+import NavBar from "./NavBar";
+
+const AppHeader = () => {
+  return (
+    <Box sx={{flexGrow: 1}} color="#A6D1C9">
+      <AppBar position="static">
+        <Toolbar>
+          <NavBar></NavBar>
+          <Typography
+            variant="h4"
+            component="div"
+            sx={{flexGrow: 1, fontWeight: "550"}}
+            align="center"
+          >
+            Todo App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default AppHeader;
---
+++ src/components/UI/NavBar.tsx
@@ -0,0 +1,61 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import {IconButton, Menu, MenuItem} from "@mui/material";
+
+import React from "react";
+import {useNavigate} from "react-router-dom";
+
+const AppNavBar = () => {
+  let navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <IconButton
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem
+          onClick={() => {
+            handleClose();
+            navigate("/todos");
+          }}
+        >
+          All Todo Lists
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            handleClose();
+            navigate("/");
+          }}
+        >
+          New Todo List
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default AppNavBar;
